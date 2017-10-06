31735b0a9bbb603ad278dd371e5ce545019a7ff1
Peter Kordel <pkordel@gmail.com>
2017-10-06 17:02:18 +0700

    Cleanup

--- app/models/refinery/redirections/redirection.rb
+++ app/models/refinery/redirections/redirection.rb
@@ -1,9 +1,7 @@
 class Refinery::Redirections::Redirection < ActiveRecord::Base
   extend Enumerize
 
-  attr_accessible :from_url, :status_code, :to_url
-
-  enumerize :status_code, in: ["301", "302", "303", "307"]
+  enumerize :status_code, in: %w[301 302 303 307]
 
   validates :from_url, :status_code, :to_url, presence: true
 
@@ -15,25 +13,35 @@ class Refinery::Redirections::Redirection < ActiveRecord::Base
 
   scope :from_url, ->(path) { where from_url: sanitize_path(path) }
 
-  # This method gets a path and 'sanitizes it'. This means that all the params that we don't want (Google Analytics params, testing params...) are stripped out and params are sorted so '/en?a=1&b=1' and '/en?b=1&a=1' are the same
-  def self.sanitize_path path, ignored_path_params = Refinery::Redirections.ignored_path_params
-    path, params = path.split("?")
+  # This method gets a path and 'sanitizes it'. This means that all the params
+  # that we don't want (Google Analytics params, testing params...) are
+  # stripped out and params are sorted so '/en?a=1&b=1' and '/en?b=1&a=1'
+  # are the same
+  def self.sanitize_path(
+    path, ignored_path_params = Refinery::Redirections.ignored_path_params
+  )
+    path, params = path.split('?')
     if params.blank?
       path
     else
       # We split the params
       params = params.split('&')
       # And then get those ignored and remove them
-      not_ignored_params = params.reject{|p| ignored_path_params.detect{|ignored| p.start_with?("#{ignored}=") || (p == ignored)}}
+      not_ignored_params = params.reject do |p|
+        ignored_path_params.detect do |ignored|
+          p.start_with?("#{ignored}=") || (p == ignored)
+        end
+      end
 
-      not_ignored_params.blank? ? path : "#{path}?#{not_ignored_params.sort.join('&')}"
-
+      if not_ignored_params.blank?
+        path
+      else
+        "#{path}?#{not_ignored_params.sort.join('&')}"
+      end
     end
-
   end
 
   def title
     "#{from_url} -> #{to_url} (#{status_code})"
   end
-
 end
